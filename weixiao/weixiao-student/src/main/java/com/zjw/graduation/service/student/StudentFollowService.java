6227e188a080e0d24aed465e5a7a7cc3f2fd0ca8
zhangjw <1617386850@qq.com>
2020-04-24 11:48:33 +0800

    init student-follow

---
+++ weixiao/weixiao-student/src/main/java/com/zjw/graduation/service/student/StudentFollowService.java
@@ -0,0 +1,24 @@
+package com.zjw.graduation.service.student;
+
+import com.zjw.graduation.data.PagingResult;
+import com.zjw.graduation.entity.student.StudentFollow;
+
+/**
+ * 
+ *
+ * @author zjw
+ * @email zhangjw9898@qq.com
+ * @date 2020-04-24 11:47:21
+ */
+public interface StudentFollowService {
+    PagingResult<StudentFollow> page(int pageIndex, int pageSize);
+
+    StudentFollow get(Long id);
+
+    StudentFollow save(StudentFollow Admin);
+
+    StudentFollow update(StudentFollow Admin);
+
+    void delete(Long id);
+}
+
